b4e215627a7f1f86a3dc5c111cf08d387f7b01ef
ygsjason <99283954+ygsjason@users.noreply.github.com>
2022-03-19 15:54:29 -0500

    Update Breslin_Oscar_Count.py

--- Breslin_Oscar_Count.py
+++ Breslin_Oscar_Count.py
@@ -4,7 +4,7 @@ import pandas as pd
 #filter Tricks
 oscar_nominees.query('id == "1" | year == "2000"')
 oscar_nominees[oscar_nominees.id.isin(["1", "2"])]
-oscar_nominees[(oscar_nominees['id'] == 1) | (oscar_nominees['id' == 2)] 
+oscar_nominees[(oscar_nominees['id'] == 1) | (oscar_nominees['id'] == 2)] 
 oscar_nominees[oscar_nominees['id'].isin([2,3])]
 oscar_nominees.query('nominee == "Abigail Breslin"').groupby('movie').size().reset_index()
 oscar_nominees.groupby('nominee').size().reset_index().query('nominee == "Abigail Breslin"')
